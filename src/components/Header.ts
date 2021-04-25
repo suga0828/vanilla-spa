43fcdb93565c351e80910c9da34f6aab5a9ec913
Alexander Sandoval <alexander.sf0828@gmail.com>
2021-04-25 12:44:56 -0500

    fix: use relatives urls

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -7,14 +7,14 @@ const Header: Component = {
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="flex items-center justify-between h-16">
             <div class="flex items-center">
-              <a class="flex-shrink-0" href="/#/">
+              <a class="flex-shrink-0" href="./#/">
                 <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
               </a>
               <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
-                  <a href="/#/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-                  <a href="/#/about" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-                  <a href="/#/404-route" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">404</a>
+                  <a href="./#/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                  <a href="./#/about" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                  <a href="./#/404-route" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">404</a>
                 </div>
               </div>
             </div>
@@ -35,9 +35,9 @@ const Header: Component = {
         <div class="md:hidden">
           <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="/#/" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
-            <a href="/#/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
-            <a href="/#/other-route" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">404</a>
+            <a href="./#/" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
+            <a href="./#/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+            <a href="./#/other-route" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">404</a>
           </div>
         </div>
       </nav>
